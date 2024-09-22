5c12fb9515f1f3a528a2677ad2ee5e88a233a647
marl0vv <23glad02@mail.ru>
2024-09-22 13:40:41 +1000

    Add Spring Initializr

---
+++ src/test/java/com/GlaMik/VacationPayCalculator/VacationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GlaMik.VacationPayCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
