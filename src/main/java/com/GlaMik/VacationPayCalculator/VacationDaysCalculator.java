a0a0b83a981fb302cb5200892e2407b117e2be6e
marl0vv <23glad02@mail.ru>
2024-09-22 21:10:16 +1000

    Count vacation days class
    - Added class that counts the number of vacation days, taking into account holidays and weekends

---
+++ src/main/java/com/GlaMik/VacationPayCalculator/VacationDaysCalculator.java
@@ -0,0 +1,67 @@
+package com.GlaMik.VacationPayCalculator;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class VacationDaysCalculator {
+
+    private LocalDate vacationStart;
+    private LocalDate vacationEnd;
+    private Set<LocalDate> nonWorkingDays;
+
+    private static final Map<LocalDate, Integer> HOLIDAYS = new HashMap<>();
+    static {
+        HOLIDAYS.put(LocalDate.of(2024, 1, 1), 8);  // Новогодние каникулы (8 дней)
+        HOLIDAYS.put(LocalDate.of(2024, 2, 23), 1); // День защитника Отечества (1 день)
+        HOLIDAYS.put(LocalDate.of(2024, 3, 8), 1);  // Международный женский день (1 день)
+        HOLIDAYS.put(LocalDate.of(2024, 5, 1), 1);  // Праздник Весны и Труда (1 день)
+        HOLIDAYS.put(LocalDate.of(2024, 5, 9), 1);  // День Победы (1 день)
+        HOLIDAYS.put(LocalDate.of(2024, 6, 12), 1); // День России (1 день)
+        HOLIDAYS.put(LocalDate.of(2024, 11, 4), 1); // День народного единства (1 день)
+    }
+
+    public VacationDaysCalculator(LocalDate vacationStart, LocalDate vacationEnd) {
+        this.vacationStart = vacationStart;
+        this.vacationEnd = vacationEnd;
+    }
+
+    public int computeVacationDays() {
+        nonWorkingDays = new HashSet<>();
+        computeHolidays();
+        computeWeekends();
+
+        long totalDays = DAYS.between(vacationStart, vacationEnd.plusDays((1)));
+        return (int) (totalDays - nonWorkingDays.size());
+    }
+
+    public void computeHolidays() {
+        for (Map.Entry<LocalDate, Integer> entry : HOLIDAYS.entrySet()) {
+            LocalDate holidayStart = entry.getKey();
+            int holidayDuration = entry.getValue();
+
+            if (holidayStart.isEqual(vacationStart) ||
+                    (holidayStart.isAfter(vacationStart) && holidayStart.isBefore(vacationEnd))) {
+                for (int i = 0; i < holidayDuration; i++) {
+                    nonWorkingDays.add(holidayStart.plusDays(i));
+                }
+            }
+        }
+    }
+
+    public void computeWeekends() {
+        LocalDate date = vacationStart;
+        while (!date.isAfter(vacationEnd)) {
+            if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                nonWorkingDays.add(date);
+            }
+            date = date.plusDays(1);
+        }
+    }
+
+}
